583a10d066e33d558735c797ea670ada2489b7cc
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2025-07-23 22:59:04 -0500

    Fixed by inability to spell

--- writing_canvas.gd
+++ writing_canvas.gd
@@ -3,20 +3,20 @@ extends Node2D
 var colors : Dictionary[String, Color] =  {"black" : Color.BLACK, "white" : Color.WHITE}
 var activeColor : String = "white"
 
-var stroakDict : Dictionary[int, Array] = {0 : []}
+var strokeDict : Dictionary[int, Array] = {0 : []}
 var vec2Array : Array[Vector2] = []
 
 
 func drawAt(posToDraw : Vector2) -> void:
 	vec2Array.append(posToDraw)
-	stroakDict.set(stroakDict.size() - 1, [vec2Array.duplicate(), activeColor])
+	strokeDict.set(strokeDict.size() - 1, [vec2Array.duplicate(), activeColor])
 	
 	drawFromPositionList() #redraw everything
 
 
 func drawEnd() -> void:
 	vec2Array.clear()
-	stroakDict.set(stroakDict.size(), [[], "transparent"])
+	strokeDict.set(strokeDict.size(), [[], "transparent"])
 
 
 ## this function clears the draw and then redraws every line in all stroak lists in the current draw position list
@@ -25,19 +25,15 @@ func drawFromPositionList() -> void:
 	await self.draw
 	
 	#draw every stroak stored in the dictionary
-	for stroak in stroakDict:
-		for drawPosition in range(0, stroakDict[stroak][0].size() - 1):
-			draw_line(stroakDict[stroak][0][drawPosition], stroakDict[stroak][0][drawPosition + 1], colors[stroakDict[stroak][1]], 5)
-	
-	#draw current stroak
-	for vec2Position in range(0, vec2Array.size() -1):
-		draw_line(vec2Array[vec2Position], vec2Array[vec2Position + 1], colors[activeColor], 5)
+	for stroke in strokeDict:
+		for drawPosition in range(0, strokeDict[stroke][0].size() - 1):
+			draw_line(strokeDict[stroke][0][drawPosition], strokeDict[stroke][0][drawPosition + 1], colors[strokeDict[stroke][1]], 5)
 
 
 func undoLastStroak():
-	if stroakDict.size() - 1 != 0:
-		stroakDict.set(stroakDict.size() - 2, stroakDict[stroakDict.size() - 1])
-		stroakDict.erase(stroakDict.size() - 1)
+	if strokeDict.size() - 1 != 0:
+		strokeDict.set(strokeDict.size() - 2, strokeDict[strokeDict.size() - 1])
+		strokeDict.erase(strokeDict.size() - 1)
 		drawFromPositionList()
 
 
@@ -50,4 +46,4 @@ func swapColor() -> void: #prob a temperary function
 		
 		"black":
 			activeColor = "white"
-			print("White")
+			print("white")
